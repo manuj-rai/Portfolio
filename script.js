dfca677a0f4d2be010bbbc19b99e4fb26f7eb714
Manuj Rai <imanujrai7@gmail.com>
2024-11-07 11:14:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+if (scrollToTopBtn) {
+    window.onscroll = function () {
+        if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    };
+
+    scrollToTopBtn.onclick = function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    };
+}
