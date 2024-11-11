56240d61cd1e6dc804f8edee0316ae22e7dbcb43
Manuj Rai <imanujrai7@gmail.com>
2024-11-11 10:24:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -23,3 +23,33 @@ if (scrollToTopBtn) {
         });
     };
 }
+
+
+// Get the dark mode toggle checkbox and body element
+const toggleCheckbox = document.querySelector('.input');
+const body = document.body;
+const sunIcon = document.getElementById('sun-icon');
+const moonIcon = document.getElementById('moon-icon');
+
+// Check if the user has previously set a theme
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+    body.classList.add('dark-mode');
+    toggleCheckbox.checked = true; // Set the checkbox to checked
+} else {
+    body.classList.remove('dark-mode');
+    toggleCheckbox.checked = false; // Set the checkbox to unchecked
+}
+
+// Toggle dark mode when checkbox is clicked
+toggleCheckbox.addEventListener('change', () => {
+    body.classList.toggle('dark-mode');
+    
+    // Save the theme preference in localStorage
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
+
